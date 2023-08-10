98c8eb7140567b3e5cb9dd7b73591954d77d2f9c
Chuck Grindel <chuck.grindel@gmail.com>
2023-08-10 15:36:14 -0600

    chore: add better which checks

--- .github/actions/run_renovate_with_bazel/fake_entrypoint.sh
+++ .github/actions/run_renovate_with_bazel/fake_entrypoint.sh
@@ -3,7 +3,7 @@
 # DEBUG BEGIN
 set -x
 
-which bazelisk
-which clang
-which renovate
+which bazelisk || echo "bazelisk not found"
+which clang || echo "clang not found"
+which renovate || echo "renovate not found"
 # DEBUG END
