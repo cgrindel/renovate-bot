31d21984c6a48541b57092a7d52febbb7382977c
Chuck Grindel <chuck.grindel@gmail.com>
2023-08-15 13:27:58 -0600

    chore: use custom Renovate Docker image (#18)

--- .github/actions/run_renovate_with_bazel/fake_entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# DEBUG BEGIN
-set -x
-
-which bazelisk || echo "bazelisk not found"
-which clang || echo "clang not found"
-which renovate || echo "renovate not found"
-# DEBUG END
